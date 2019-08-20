149912d909fda76c6866497e4161bf364044abc7
take1931032@gmail.com <take1931032@gmail.com>
2019-08-20 16:09:00 +0900

    update

--- src/main.go
+++ src/main.go
@@ -7,9 +7,11 @@ import(
   "sync"
   "text/template"
   "fmt"
+  "io/ioutil"
+  // "os"
+  // // "database/sql"
+  // _ "github.com/lib/pq"
 )
-// "database/sql"
-// _ "github.com/lib/pq"
 
 type templateHandler struct {
   once sync.Once
@@ -30,6 +32,15 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // DB周り
 func main() {
   fmt.Println("hello  ʕ◔ϖ◔ʔ")
+  
+  raw, err := ioutil.ReadFile("./json/profile.json")
+  if err != nil {
+      fmt.Println(err.Error())
+      // os.Exit(1)
+  }else{
+    fmt.Println(raw)
+  }
+
   http.Handle("/", &templateHandler{filename: "chat.html"})
 
   if err := http.ListenAndServe(":8080", nil); err != nil {
